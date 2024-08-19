b22f1bea9f780022003a856251bce8a6f1f0945a
Marek Zelinka <mzelinka17@gmail.com>
2024-08-19 11:36:52 +0200

    create empty contact

--- app/root.tsx
+++ app/root.tsx
@@ -1,14 +1,31 @@
+import { json } from '@remix-run/node'
 import {
+  Form,
+  Link,
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from '@remix-run/react'
 import type { ReactNode } from 'react'
+import { createEmptyContact, getContacts } from './contacts'
 import './index.css'
 
+export async function loader() {
+  const contacts = await getContacts()
+  return json({ contacts })
+}
+
+export async function action() {
+  const contact = await createEmptyContact()
+  return json({ contact })
+}
+
 export function Layout({ children }: { children: ReactNode }) {
+  const { contacts } = useLoaderData<typeof loader>()
+
   return (
     <html lang="en">
       <head>
@@ -18,7 +35,52 @@ export function Layout({ children }: { children: ReactNode }) {
         <Links />
       </head>
       <body>
-        <div id="app">{children}</div>
+        <div id="sidebar">
+          <h1>Remix Contacts</h1>
+          <div>
+            <Form id="search-form" role="search">
+              <input
+                id="q"
+                aria-label="Search contacts"
+                placeholder="Search"
+                type="search"
+                name="q"
+              />
+              <div id="search-spinner" aria-hidden hidden={true} />
+            </Form>
+            <Form method="post">
+              <button type="submit">New</button>
+            </Form>
+          </div>
+          <nav>
+            {contacts.length ? (
+              <ul>
+                {contacts.map((contact) => (
+                  <li key={contact.id}>
+                    <Link to={`/contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{' '}
+                      {contact.favorite ? <span>★</span> : null}
+                    </Link>
+                  </li>
+                ))}
+                <li>
+                  <Link to={`/contacts/2`}>Your Friend</Link>
+                </li>
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+              </p>
+            )}
+          </nav>
+        </div>
+        <div id="detail">{children}</div>
         <ScrollRestoration />
         <Scripts />
       </body>
